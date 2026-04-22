55bf65b99c940af51e45188d5a9ac173c318776f
tunglinkn <tunga6490@gmail.com>
2026-04-22 17:42:11 +0700

    Revert "dư thừa"
    This reverts commit a714bbf49e6166e27f4f91c03527c6e2e700f609.

--- src/model/Phim.java
+++ src/model/Phim.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class Phim {
     private String maPhim;
@@ -11,11 +12,9 @@ public class Phim {
     private int thoiLuongPhim; // in minutes
     private String loaiPhim;
 
-    public Phim() {
-    }
+    public Phim() {}
 
-    public Phim(String maPhim, String tenPhim, LocalDate ngaySanXuat, String donViSanXuat, int gioiHan,
-            int thoiLuongPhim, String loaiPhim) {
+    public Phim(String maPhim, String tenPhim, LocalDate ngaySanXuat, String donViSanXuat, int gioiHan, int thoiLuongPhim, String loaiPhim) {
         this.maPhim = maPhim;
         this.tenPhim = tenPhim;
         this.ngaySanXuat = ngaySanXuat;
@@ -26,62 +25,20 @@ public class Phim {
     }
 
     // Getters and Setters
-    public String getMaPhim() {
-        return maPhim;
-    }
+    public String getMaPhim() { return maPhim; }
+    public void setMaPhim(String maPhim) { this.maPhim = maPhim; }
+    public String getTenPhim() { return tenPhim; }
+    public void setTenPhim(String tenPhim) { this.tenPhim = tenPhim; }
+    public LocalDate getNgaySanXuat() { return ngaySanXuat; }
+    public void setNgaySanXuat(LocalDate ngaySanXuat) { this.ngaySanXuat = ngaySanXuat; }
+    public String getDonViSanXuat() { return donViSanXuat; }
+    public void setDonViSanXuat(String donViSanXuat) { this.donViSanXuat = donViSanXuat; }
+    public int getGioiHan() { return gioiHan; }
+    public void setGioiHan(int gioiHan) { this.gioiHan = gioiHan; }
+    public int getThoiLuongPhim() { return thoiLuongPhim; }
+    public void setThoiLuongPhim(int thoiLuongPhim) { this.thoiLuongPhim = thoiLuongPhim; }
+    public String getLoaiPhim() { return loaiPhim; }
+    public void setLoaiPhim(String loaiPhim) { this.loaiPhim = loaiPhim; }
 
-    public void setMaPhim(String maPhim) {
-        this.maPhim = maPhim;
-    }
-
-    public String getTenPhim() {
-        return tenPhim;
-    }
-
-    public void setTenPhim(String tenPhim) {
-        this.tenPhim = tenPhim;
-    }
-
-    public LocalDate getNgaySanXuat() {
-        return ngaySanXuat;
-    }
-
-    public void setNgaySanXuat(LocalDate ngaySanXuat) {
-        this.ngaySanXuat = ngaySanXuat;
-    }
-
-    public String getDonViSanXuat() {
-        return donViSanXuat;
-    }
-
-    public void setDonViSanXuat(String donViSanXuat) {
-        this.donViSanXuat = donViSanXuat;
-    }
-
-    public int getGioiHan() {
-        return gioiHan;
-    }
-
-    public void setGioiHan(int gioiHan) {
-        this.gioiHan = gioiHan;
-    }
-
-    public int getThoiLuongPhim() {
-        return thoiLuongPhim;
-    }
-
-    public void setThoiLuongPhim(int thoiLuongPhim) {
-        this.thoiLuongPhim = thoiLuongPhim;
-    }
-
-    public String getLoaiPhim() {
-        return loaiPhim;
-    }
-
-    public void setLoaiPhim(String loaiPhim) {
-        this.loaiPhim = loaiPhim;
-    }
-
-    public void layChiTietPhim() {
-        /* In thông tin chi tiết */ }
+    public void layChiTietPhim() { /* In thông tin chi tiết */ }
 }
